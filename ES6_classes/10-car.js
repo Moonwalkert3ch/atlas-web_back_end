7df2015760bbc6667535622a6a64882913d4e76c
Moonwalkert3ch <6620@holbertonstudents.com>
2024-01-04 23:31:22 -0600

    func valued prperty

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,41 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const newObject = this.constructor[Symbol.species];
+
+    return new newObject();
+  }
+}
