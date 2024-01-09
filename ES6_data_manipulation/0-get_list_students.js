bd95d078c1a7515019393c9d1c0125d16effbda8
Moonwalkert3ch <6620@holbertonstudents.com>
2024-01-09 17:52:08 -0600

    fixed typo on 5 id

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
+// create a function
 export default function getListStudents() {
+  // return attributes of created array
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
