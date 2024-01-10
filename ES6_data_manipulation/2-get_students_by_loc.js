832f78c587940c3b0cd937e9fccc0cb3c4ad47d0
Moonwalkert3ch <6620@holbertonstudents.com>
2024-01-09 18:38:01 -0600

    added space in comment

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -3,6 +3,6 @@ export default function getStudentsByLocation(students, city) {
   // filter function gets students in specified city
   const studentList = students.filter((student) => student.location === city);
 
-  //return the array of students in that city
+  // return the array of students in that city
   return studentList;
 }
