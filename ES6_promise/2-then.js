8e03af06f720baf32bec2409f82919c4be90755b
Moonwalkert3ch <6620@holbertonstudents.com>
2024-01-10 21:11:28 -0600

    correct eslint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 // append three handlers to the function
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(response => {
+    .then((response) => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: success,
       };
     })
-    .catch(error => {
+    .catch((error) => {
       return new Error();
     });
 }
