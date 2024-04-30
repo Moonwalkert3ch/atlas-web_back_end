39e0389b9bb7bbcfcd9a5f64a9a65c2b41eba020
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 16:51:16 -0500

    shorten description in line was too long

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Defines a function that takes a float as argument and returns the string representation of the float"""
+"""Defines a function that takes a float as argument and 
+returns the string representation of the float"""
 
 
 def to_str(n: float) -> str:
