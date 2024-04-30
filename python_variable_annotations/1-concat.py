c3b2600ff4b636a240d7bd1250a1ca66bd678742
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 14:35:44 -0500

    concat strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a function that concatenates 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the strings concatenated
+    Param Arg1: str1 - a string
+    Arg2: str2 - second string"""
+
+    return str1 + str2
