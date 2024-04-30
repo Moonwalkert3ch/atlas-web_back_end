81ca4b7de73ac4f8e9004413457b5f6c51de517f
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 14:54:04 -0500

    find the floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import math
+"""Defines a function that returns the floor of the float"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor
+    Param Arg: n - a float"""
+
+    return math.floor(n)
