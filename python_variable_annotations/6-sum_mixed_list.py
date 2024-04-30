300763c0d60183d2d3d0057363f3fd2a0f24d02f
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 17:43:48 -0500

    removed whitespace

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -9,8 +9,8 @@ from typing import List, Union
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of the list
-    Param Arg1: mxd_list - list of mixed int and float 
-    numbers
+    Param Arg1: mxd_list - list of mixed int and float
+    numbers.
     Return: sum as a float
     """
 
