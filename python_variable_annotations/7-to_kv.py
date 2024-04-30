bc68625f44a5fef11a0b8cf2158a4ac700bc4eac
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 18:03:10 -0500

    returninf as a tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines a function that takes a string and an
+int or float as args and returns as a tuple """
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """
+    Returns a string key and an int/float value to 
+    a tuple.
+    Parameter
+    Args1: k(str) - string key
+    V(int, float) - integer or float value
+    Return: tuple with arg values
+    """
+
+    return k, float(v ** 2)
