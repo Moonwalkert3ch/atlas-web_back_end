cc9878bc406df6c73c61a41d7edb70c3706abc61
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 14:18:59 -0500

    added comment

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+""" Defines a function that adds 2 floats """
 
 
 def add(a: float, b: float) -> float:
-    ''' arg1: a - first float number
+    """ Param arg1: a - first float number
     arg2: b - second float number
-    return: sum of args'''
+    return: sum of args"""
 
     return a + b
