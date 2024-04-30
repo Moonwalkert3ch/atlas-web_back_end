c35729339ae8949cd17301763329d3dd4ce7ebdf
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 18:07:14 -0500

    remove whitespace line 10

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a string key and an int/float value to 
+    Returns a string key and an int/float value to
     a tuple.
     Parameter
     Args1: k(str) - string key
