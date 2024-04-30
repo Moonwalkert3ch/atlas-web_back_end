c91f75c0617f90745abcc980e848cc47b6a4823d
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 14:05:37 -0500

    return float sum

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    ''' arg1: a - first float number
+    arg2: b - second float number
+    return: sum of args'''
+
+    return a + b
