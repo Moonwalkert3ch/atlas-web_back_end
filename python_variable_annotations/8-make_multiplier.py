2c2d28791971e3d633fd8b83e113fab2d085260f
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 20:44:02 -0500

    removed whitespace

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Defines a function that takes a float as an 
+"""Defines a function that takes a float as an
 argument and returns it multiplied by a float"""
 
 from typing import Callable
@@ -7,7 +7,6 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a multiplier function.
-    
     Parameter Arg1: multiplier(float) - multiplier value.
     Returns: Callable[[float], float] - float multiplied"""
 
