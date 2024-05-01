60e90f796a4b68850865dbbb8b4892a98d63176d
Moonwalkert3ch <6620@holbertonstudents.com>
2024-04-30 21:02:53 -0500

    added sequence

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,12 +2,12 @@
 """Defines a function that iterates through
 the input list"""
 
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Iterable) -> List[Tuple]:   
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:   
     """Returns the values in the list
-    Parameters Args: lst(Iterable) - the iterable object.
-    Returns: List[Tuple] - the list of tuples"""
+    Parameters Args: lst(Iterable[Sequence]) - the iterable object.
+    Returns: List[Tuple[Sequence, int]] - the list of tuples"""
 
     return [(i, len(i)) for i in lst]
