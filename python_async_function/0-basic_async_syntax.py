baac751b7fc6187d280d4f15298b57dbeaba8250
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-03 13:19:12 -0500

    added more notes

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,18 +5,11 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: float = 10) -> float:
     """Returns a random delay between 0 and max_delay
-    Paramater Args: max_delay(int) - is the max delay amount
+    Paramater Args: max_delay(float) - is the max delay amount
     Return: random number between 0 and 10
     """
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-async def main():
-    """Create entry point function"""
-    random_delay = await wait_random()
-    print(f"Waited for {random_delay} seconds")
-
-asyncio.run(main())
