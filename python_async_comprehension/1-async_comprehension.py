52ce4edbe92d23ad2be74eeb60d97650d27e1c16
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-04 17:44:28 -0500

    noticed i had lower case list instead of List usingso it wasnt using the List()

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,16 +7,16 @@ async_generator, then return the 10 random numbers.
 
 import asyncio
 import random
-from typing import Generator, List
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """
     Returns a coroutine that will collect 10 random numbers
     using an async comprehensing over async_generator, then
     return the 10 random numbers
     Return(float) - List of 10 random numbers.
     """
-    return [i async for i in async_generator()]
+    return [numbers async for numbers in async_generator()]
