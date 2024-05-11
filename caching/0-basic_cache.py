199b5712793b165821a823c541ddbc2aae47c817
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-11 17:12:08 -0500

    added more comments explaining each class and method

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -6,14 +6,20 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Create a class BasicCache that inherits from
-    BaseCaching and is a caching system"""
+    """
+    Create a class BasicCache that inherits from
+    BaseCaching and is a caching system
+    Attributes: put - method that adds key/value to item
+    get - method to store the cache in key/value
+    """
 
     def put(self, key, item):
+        """Adds key/value pair to item"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Retrieves the value stored in key cache"""
         if key is not None:
             return self.cache_data.get(key)
         return None
