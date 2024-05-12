989ea6951f13f9ba2a553d07b70702c60d235694
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-12 17:11:24 -0500

    fix pycode

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,7 +8,11 @@ returns a tuple of size two of start/end index
 
 def index_range(page, page_size):
     """
-    function should return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.
+    function should return a tuple of size two
+    containing a start index and an end index
+    corresponding to the range of indexes to
+    return in a list for those particular
+    pagination parameters.
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
