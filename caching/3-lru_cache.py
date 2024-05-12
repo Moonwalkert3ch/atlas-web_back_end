9785cf7f3ebbf8ce212ee952acb5fbfcd940c59d
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-12 15:11:21 -0500

    was not printing t=in the order inserted

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -17,21 +17,24 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """Adds the key/value to cache data"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                if self.order:
-                    discard_key = self.order.pop(0)
-                    del self.cache_data[discard_key]
-                    print(f"DISCARD: {discard_key}")    
-            else:
-                self.order.append(key)
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.order.pop(0)
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+
+        self.order.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
         """Returns the value linked to the key"""
-        if key is not None and key in self.cache_data:
-            if key in self.order:
-                self.order.remove(key)
-            self.order.append(key)
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+             return None
+
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
