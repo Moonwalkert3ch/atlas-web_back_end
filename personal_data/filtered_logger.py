e4de7b276c86d832772fb7ae5cd2933170497678
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-18 20:10:09 -0500

    revise documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,18 +7,18 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
-    """Arguments: fields: a list of strings
+    """
+    Param Arguments:
+    fields(list(str)): a list of strings
     representing all fields to obfuscate
     redaction: a string representing by what
     the field will be obfuscated 
-    message: a string representing the log line
-    separator: a string representing by which
+    message(str): a string representing the log line
+    separator(str): a string representing by which
     character is separating all fields in the log
-    line (message) The function should use a regex
-    to replace occurrences of certain field values.
-    filter_datum should be less than 5 lines long
-    and use re.sub to perform the substitution with
-    a single regex."""
+    line (message)
+    Return: a regex to replace occurrences of certain
+    field values."""
     for field in fields:
         # create the regex pattern for the field
         field_pattern = f"{re.escape(field)}=[^{re.escape(seperator)}]*"
