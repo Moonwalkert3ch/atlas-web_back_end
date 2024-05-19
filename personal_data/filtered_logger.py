646602f8ea224c042b6c8297197ebf76e11b5a5a
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-18 20:44:02 -0500

    no traceback results are showing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -22,24 +22,20 @@ class RedactingFormatter(logging.Formatter):
         else:
             self.fields = fields
 
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-    def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+    def filter_datum(self, message: str) -> str:
         """
         Writes a function that returns the log message obfuscated
         Param Arguments:
         fields(list(str)): a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
-        message(str): a string representing the log line
-        separator(str): a string representing by which character is
-        separating all fields in the log line
-        Return: a regex to replace occurrences of certain field values.
+        Return(str): Values for fields in fields should be filtered..
         """
-        for field in fields:
+        for field in self.fields:
             # create the regex pattern for the field
-            field_pattern = f"{re.escape(field)}=[^{re.escape(separator)}]*"
+            field_pattern = f"{re.escape(field)}=[^;]*"
             # then i substitute the field with the redaction
-            message = re.sub(field_pattern, f"{field}={redaction}", message)
+            message = re.sub(field_pattern, f"{field}={self.REDACTION}", message)
         return message
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = self.filter_datum(record.msg)
+        return super().format(record)
