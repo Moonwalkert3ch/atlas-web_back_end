d6b78d21f0103c6477e432d6ce67bc4e3b9e1aa6
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-18 20:21:38 -0500

    fixed new pycode error

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -14,12 +14,13 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     fields(list(str)): a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message(str): a string representing the log line
-    separator(str): a string representing by which character is separating all fields in the log line
+    separator(str): a string representing by which character is
+    separating all fields in the log line
     Return: a regex to replace occurrences of certain field values.
     """
     for field in fields:
         # create the regex pattern for the field
-        field_pattern = f"{re.escape(field)}=[^{re.escape(seperator)}]*"
+        field_pattern = f"{re.escape(field)}=[^{re.escape(separator)}]*"
         # then i substitute the field with the redaction
         message = re.sub(field_pattern, f"{field}={redaction}", message)
     return message
