74150a2aea9c952de4c2042917fd86cb84f4c1f0
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-18 21:28:20 -0500

    pycode error on line 48

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -27,8 +27,10 @@ class RedactingFormatter(logging.Formatter):
         """
         Writes a function that returns the log message obfuscated
         Param Arguments:
-        fields(list(str)): a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
+        fields(list(str)): a list of strings representing all fields
+        to obfuscate
+        redaction: a string representing by what the field will be
+        obfuscated
         message(str): a string representing the log line
         separator(str): a string representing by which character is
         separating all fields in the log line
@@ -38,7 +40,8 @@ class RedactingFormatter(logging.Formatter):
             # create the regex pattern for the field
             field_pattern = f"{re.escape(field)}=[^;]*"
             # then i substitute the field with the redaction
-            message = re.sub(field_pattern, f"{field}={self.REDACTION}", message)
+            message = re.sub(field_pattern, f"{field}={
+                             self.REDACTION}", message)
         return message
 
     def format(self, record: logging.LogRecord) -> str:
