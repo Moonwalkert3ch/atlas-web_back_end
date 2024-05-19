70d0aaae4eb196493501f3600d2288161c2f7b36
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-18 20:18:04 -0500

    fix pycode errors

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,23 +6,21 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 seperator: str) -> str:
     """
+    Writes a function that returns the log message obfuscated
     Param Arguments:
-    fields(list(str)): a list of strings
-    representing all fields to obfuscate
-    redaction: a string representing by what
-    the field will be obfuscated 
+    fields(list(str)): a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
     message(str): a string representing the log line
     separator(str): a string representing by which
-    character is separating all fields in the log
-    line (message)
-    Return: a regex to replace occurrences of certain
-    field values."""
+    character is separating all fields in the log line (message)
+    Return: a regex to replace occurrences of certain field values.
+    """
     for field in fields:
         # create the regex pattern for the field
         field_pattern = f"{re.escape(field)}=[^{re.escape(seperator)}]*"
         # then i substitute the field with the redaction
         message = re.sub(field_pattern, f"{field}={redaction}", message)
-
     return message
