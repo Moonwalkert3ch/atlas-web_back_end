188967ea1eaa98ef63323b7df7458d06c24a4dd2
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-18 20:20:12 -0500

    misspelled separtor so it was type none. fixed

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,15 +7,14 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
+                 separator: str) -> str:
     """
     Writes a function that returns the log message obfuscated
     Param Arguments:
     fields(list(str)): a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message(str): a string representing the log line
-    separator(str): a string representing by which
-    character is separating all fields in the log line (message)
+    separator(str): a string representing by which character is separating all fields in the log line
     Return: a regex to replace occurrences of certain field values.
     """
     for field in fields:
