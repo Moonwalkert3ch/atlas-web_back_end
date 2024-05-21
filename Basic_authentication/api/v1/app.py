014e7f5360ad45acba3ac36b2e3c254e17f1d7d6
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-20 21:33:13 -0500

    create forbidden handler

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ for a request unauthorized
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """ for a request unauthorized
