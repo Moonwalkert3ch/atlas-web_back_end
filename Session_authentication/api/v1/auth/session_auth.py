98525521a3105777ddadc1a63e29ba79ffc760a4
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-24 19:34:42 -0500

    create session instance

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,24 @@
 
 from api.v1.auth.auth import Auth
 import base64
+import uuid
+from typing import List
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """empty class"""
+  
+    user_id_by_session_id = {}
+    """session auth manages session ids"""
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user id.
+        Parameter Args:
+        user_id - id of session user
+        Returns(str) -  session id if valid
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
