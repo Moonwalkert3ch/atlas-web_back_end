acee4c2c1680a98067cb8f7fb0432232a3a650e8
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-24 19:46:37 -0500

    return cookies value

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -41,3 +42,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Public method that returns none"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = os.getenv("SESSION_NAME")
+        return request.cookies.get(session_name)
