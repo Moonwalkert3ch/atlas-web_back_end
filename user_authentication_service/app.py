1036579a3251c4164d3c74821e0f65b8bdf73244
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-26 13:47:30 -0500

    create basic flask

---
+++ user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""create a basic flask app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def first_message():
+    message = {"message": "Bienvenue"}
+    return flask.jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
