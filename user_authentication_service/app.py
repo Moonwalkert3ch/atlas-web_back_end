b612b69dca9b61be1e748a32f02dd9797c8ba7b6
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-26 16:28:02 -0500

    fork on redirect

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -89,11 +89,11 @@ def profile() -> str:
     with 200 if found"""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-
     if user:
         return jsonify({"email": user.email}), 200
     else:
         abort(403)
+
 
 
 if __name__ == "__main__":
