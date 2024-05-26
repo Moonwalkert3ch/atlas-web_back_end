deb6a2b5a40c4da899a1dd4d6c38843578eae8e8
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-26 15:57:54 -0500

    destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -92,6 +92,14 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """updates the users session id to none"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            raise NoResultFound
+
 
 def _generate_uuid() -> str:
     """
