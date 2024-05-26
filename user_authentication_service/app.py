20b9f839cf296a352ecbedd77c1d586c4b6a9494
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-26 13:59:05 -0500

    register user method

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,9 +2,14 @@
 """create a basic flask app"""
 import flask
 from flask import Flask
+from auth import Auth
+from flask import request
+from flask import abort
+from flask import jsonify
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -15,3 +20,20 @@ def first_message():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
+@app.route('/users', methods=['POST'])
+def user_registration():
+    """Looks for email and password"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    message = {"email": "<registered email>", "message": "user created"}
+    return jsonify(message)
