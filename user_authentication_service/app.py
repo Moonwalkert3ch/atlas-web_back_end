24c4a8f95659c25a27a04f909955baaa8fb40177
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-26 14:02:16 -0500

    fix pycode error

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -21,6 +21,7 @@ def first_message():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
 
+
 @app.route('/users', methods=['POST'])
 def user_registration():
     """Looks for email and password"""
