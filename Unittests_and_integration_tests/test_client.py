1476cf86b4606a723e0d6a1fc48a46fc35af9c24
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-29 21:05:43 -0500

    line too long

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -47,7 +47,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ]
         mock_get_json.return_value = mock_repos_payload
 
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock_public_repos_url:
             mock_public_repos_url.return_value = expected_repos_url
 
             client = GithubOrgClient("test_org")
