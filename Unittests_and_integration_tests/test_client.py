9e65ab2fe5ee42313adafba13f21918d3a7086b2
Moonwalkert3ch <6620@holbertonstudents.com>
2024-05-29 21:01:42 -0500

    parameterize

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -57,3 +57,11 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with(expected_repos_url)
+
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license, expected):
+        """ test the license checker """
+        self.assertEqual(GithubOrgClient.has_license(repo, license), expected)
