11b25a17efdc8c2481c9d077b7816da67a5a6ee6
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-05 17:28:56 -0500

    trying to resolve type error issue

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -36,7 +36,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[callable]) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
         """converts data back to desired format
         Param Args: key(str) - key to convert
         fn(optional(callable)) - optional function to convert
