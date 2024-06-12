6af2f361f83e7cc5aaeb574c0c3650d7cb1b4c37
Moonwalkert3ch <rachael.bradford@atlasschool.com>
2024-06-12 01:04:30 +0000

    i have not null results

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,11 +2,11 @@
 -- bands will be ranked by their longevity
 SELECT
 	band_name,
-	IFNULL(split, CURDATE()) - formed as lifespan
+	IFNULL(split, 2024) - formed AS lifespan
 FROM
 	metal_bands
 WHERE
-	FIND_IN_SET('Glam rock', style)
+	FIND_IN_SET('Glam rock', style) > 0
 ORDER BY
 	lifespan DESC;
 
