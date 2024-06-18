dbcaf8a005b2b165d246c1891dfd00054c9c53fd
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-17 21:03:48 -0500

    async readfile version

---
+++ Node_JS/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs/promises');
+
+async function countStudents(path) {
+    try {
+        const data = await fs.readFile(path, 'utf8');
+        const rows = data.trim().split('\n');
+
+        const fields = {};
+        
+        // Iterate through each row minus header
+        rows.slice(1).forEach(row => {
+            const [firstname, lastname, age, field] = row.split(',');
+
+            if (!fields[field]) {
+                fields[field] = [];
+            }
+
+            fields[field].push(firstname);
+        });
+        
+        // Calculates number_of_students number of students
+        const number_of_students = Object.values(fields).reduce((newStudents, currentStudent) => newStudents + currentStudent.length, 0);
+        
+        console.log(`Number of students: ${number_of_students}`);
+        
+        // number of students in each field
+        for (const field in fields) {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+        
+    } catch (err) {
+        console.error('Cannot load the database');
+        throw err;
+    }
+}
+
+module.exports = countStudents;
