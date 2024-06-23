fc67e2f0cf90db91d80a204984f5c34b36fb1bb8
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-23 17:01:21 -0500

    added a few extra cases

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -17,5 +17,17 @@ describe('calculateNumber', () => {
     it('should return 6 when inputs are 1.5 and 3.7', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+
+    it('should return 5 when inputs are 1.5 and 3.2', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.2), 5);
+    });
+
+    it('should return 0 when inputs are 0 and 0', () => {
+        assert.strictEqual(calculateNumber(0, 0), 0);
+    });
+
+    it('should return -5 when inputs are -1.5 and -3.7', () => {
+        assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
+    });
 });
 
