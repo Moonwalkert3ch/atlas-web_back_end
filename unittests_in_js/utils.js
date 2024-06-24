b95bcad2046f06abebd5adbd03bbfaa1f2c66df5
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-23 21:01:25 -0500

    fix braces for utils

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,27 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+        a = Math.round(a);
+        b = Math.round(b);
+        
+        const operators = {
+            'SUM': () => a + b,
+            'SUBTRACT': () => a - b,
+            'DIVIDE': () => {
+                if (b === 0) {
+                    return 'Error';
+                }
+                return a / b;
+            }
+        };
+
+    // Checks if the type is an operator, if its not will return 'Invalid type'
+    if (type in operators) {
+        return operators[type](); // Executes the operation function
+    } else {
+        return 'Invalid type';
+    }
+    }
+
+};
+
+module.exports = Utils;
