d9acc701f37d3265f7beadb321285cf8e169f8bf
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-24 20:35:48 -0500

    remove app module at the top

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,10 +1,9 @@
 const { expect } = require('chai');
 const request = require('request');
-const app = require('./api');
 
 describe('Index page', () => {
     it('should return status 200', (done) => {
-        request.get('http://localhost:7865', (error, response, body) => {
+        request('http://localhost:7865', (error, response) => {
             expect(response.statusCode).to.equal(200);
             done();
         });
