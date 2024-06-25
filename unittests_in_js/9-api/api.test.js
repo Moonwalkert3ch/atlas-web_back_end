2eb65486b20a4af113338a695f3240e0b68a9a87
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-24 21:27:28 -0500

    not passing checker will speak with peers and tomas more on this

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,7 +1,20 @@
 const request = require('request');
 const { expect } = require('chai');
+const app = require('./api');
+const http = require('http');
+
+let server;
 
 describe('Index page', () => {
+    before((done) => {
+        server = http.createServer(app);
+        server.listen(7865, done);
+    });
+
+    after((done) => {
+        server.close(done);
+    });
+
     it('returns status 200', (done) => {
         request('http://localhost:7865', (err, res) => {
             expect(res.statusCode).to.equal(200);
@@ -18,22 +31,31 @@ describe('Index page', () => {
 });
 
 describe('Cart page', () => {
+    before((done) => {
+        server = http.createServer(app);
+        server.listen(7866, done);  // Use a different port for this test suite
+    });
+
+    after((done) => {
+        server.close(done);
+    });
+
     it('returns status 200 for valid cart ID', (done) => {
-        request('http://localhost:7865/cart/123', (err, res) => {
+        request('http://localhost:7866/cart/123', (err, res) => {
             expect(res.statusCode).to.equal(200);
             done();
         });
     });
 
     it('returns correct message for valid cart ID', (done) => {
-        request('http://localhost:7865/cart/123', (err, res, body) => {
+        request('http://localhost:7866/cart/123', (err, res, body) => {
             expect(body).to.equal('Payment methods for cart 123');
             done();
         });
     });
 
     it('returns status 404 for invalid cart ID', (done) => {
-        request('http://localhost:7865/cart/abc', (err, res) => {
+        request('http://localhost:7866/cart/abc', (err, res) => {
             expect(res.statusCode).to.equal(404);
             done();
         });
