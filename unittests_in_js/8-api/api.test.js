e1aa8775ea7ec66c1fc21f3ef2825b4a72adecc2
Moonwalkert3ch <6620@holbertonstudents.com>
2024-06-24 20:33:34 -0500

    added .get method to test since im not passing checker but i am passing the test file

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -4,14 +4,14 @@ const app = require('./api');
 
 describe('Index page', () => {
     it('should return status 200', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
+        request.get('http://localhost:7865', (error, response, body) => {
             expect(response.statusCode).to.equal(200);
             done();
         });
     });
 
     it('should return correct message', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
+        request.get('http://localhost:7865', (error, response, body) => {
             expect(body).to.equal('Welcome to the payment system');
             done();
         });
